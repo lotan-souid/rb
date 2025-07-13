2aef996d9cbc93013ad3af07a64ca5adb839e53f
lotan souid <lotan.souid@gmail.com>
2025-07-13 18:40:30 +0000

    create cluster and region

--- rb/rb/doctype/clusters/clusters.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2025, lotan suid and contributors
-// For license information, please see license.txt
-
-// frappe.ui.form.on("Clusters", {
-// 	refresh(frm) {
-
-// 	},
-// });
--- rb/rb/doctype/clusters/clusters.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2025, lotan suid and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-
-class Clusters(Document):
-	pass
