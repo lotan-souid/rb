11d41298c1646e47116a98d6fcb64e0360b8b3e9
lotan souid <lotan.souid@gmail.com>
2025-07-16 17:39:16 +0000

    before build custom crm for rb

--- rb/rb/doctype/test_map/test_map.py
+++ rb/rb/doctype/test_map/test_map.py
@@ -1,9 +1,9 @@
-# Copyright (c) 2025, lotan souid and contributors
+# Copyright (c) 2025, lotan suid and contributors
 # For license information, please see license.txt
 
 # import frappe
 from frappe.model.document import Document
 
 
-class Lots(Document):
+class TestMap(Document):
 	pass
--- rb/rb/doctype/test_map/test_test_map.py
+++ rb/rb/doctype/test_map/test_test_map.py
@@ -1,9 +1,9 @@
-# Copyright (c) 2025, lotan souid and Contributors
+# Copyright (c) 2025, lotan suid and Contributors
 # See license.txt
 
 # import frappe
 from frappe.tests.utils import FrappeTestCase
 
 
-class TestLots(FrappeTestCase):
+class TestTestMap(FrappeTestCase):
 	pass
